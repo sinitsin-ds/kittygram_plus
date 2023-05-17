459ef4507e5ec15960a88bacc5bebfaa570f4edc
Дмитрий <sinitsin-ds@yandex.ru>
2023-05-17 20:48:44 +0300

    работаем со связными моделями

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,10 +1,26 @@
 from rest_framework import serializers
 
-from .models import Cat
+from .models import Cat, Owner, Achievement
 
 
 class CatSerializer(serializers.ModelSerializer):
+    owner = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner')
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Owner
+        fields = ('first_name', 'last_name', 'cats')
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Achievement
+        fields = ('id', 'name') 
