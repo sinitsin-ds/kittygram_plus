c23f4311e697df2f8800d61d1bae3fbb046cb1e1
Дмитрий Синицын <sinitsin-ds@yandex.ru>
2023-05-21 10:16:17 +0300

    добавлена JWT автризация

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -13,5 +13,7 @@ router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
 
