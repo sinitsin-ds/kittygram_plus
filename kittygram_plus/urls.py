13cbd84bcfce71304d90caa8f3e1b7c4eabfec6d
Дмитрий Синицын <sinitsin-ds@yandex.ru>
2023-05-21 09:13:33 +0300

    добавлена аунтификация

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from rest_framework.authtoken import views
 
 from rest_framework.routers import DefaultRouter
 
@@ -12,5 +13,6 @@ router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
 
